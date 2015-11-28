29ba05884c22007831479d231af135d45e6a5802
Michal <michal.drabik0@gmail.com>
2015-11-28 13:33:09 +0100

    Simplify sample app

--- sample/src/main/java/com/michaldrabik/tapbarmenu/MainActivity.java
+++ sample/src/main/java/com/michaldrabik/tapbarmenu/MainActivity.java
@@ -2,8 +2,8 @@ package com.michaldrabik.tapbarmenu;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
-import android.widget.TextView;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -12,7 +12,6 @@ import com.michaldrabik.tapbarmenulib.TapBarMenu;
 public class MainActivity extends AppCompatActivity {
 
   @Bind(R.id.tapBarMenu) TapBarMenu tapBarMenu;
-  @Bind(R.id.label) TextView label;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -22,28 +21,23 @@ public class MainActivity extends AppCompatActivity {
 
   @OnClick(R.id.tapBarMenu) public void onMenuButtonClick() {
     tapBarMenu.toggle();
-    label.setText(tapBarMenu.isOpened() ? "Menu opened" : "Menu closed");
   }
 
   @OnClick({ R.id.item1, R.id.item2, R.id.item3, R.id.item4 }) public void onMenuItemClick(View view) {
+    tapBarMenu.close();
     switch (view.getId()) {
       case R.id.item1:
-        label.setText("Item 1 selected");
+        Log.i("TAG", "Item 1 selected");
         break;
       case R.id.item2:
-        label.setText("Item 2 selected");
+        Log.i("TAG", "Item 2 selected");
         break;
       case R.id.item3:
-        label.setText("Item 3 selected");
+        Log.i("TAG", "Item 3 selected");
         break;
       case R.id.item4:
-        label.setText("Item 4 selected");
+        Log.i("TAG", "Item 4 selected");
         break;
     }
-    tapBarMenu.close();
-  }
-
-  @Override protected void onDestroy() {
-    super.onDestroy();
   }
 }
